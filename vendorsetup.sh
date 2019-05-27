021bf3c32752ccc6868584cd21bf44d8be268099
Alisson Lauffer <alissonvitortc@gmail.com>
2019-05-27 02:30:24 +0000

    Kraken bring up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,6 @@
 # limitations under the License.
 #
 
-add_lunch_combo lineage_sanders-eng
-add_lunch_combo lineage_sanders-userdebug 
-add_lunch_combo lineage_sanders-user
+add_lunch_combo aosp_sanders-eng
+add_lunch_combo aosp_sanders-userdebug 
+add_lunch_combo aosp_sanders-user
